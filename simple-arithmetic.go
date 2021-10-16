1c77b4075fa8920592eb92a49df0355e20e9dc27
ZERO-30 <92652770+ZERO-30@users.noreply.github.com>
2021-10-17 05:34:55 +0700

    simple arithmetic
    Signed-off-by: ZERO-30 <92652770+ZERO-30@users.noreply.github.com>

---
+++ simple-arithmetic.go
@@ -0,0 +1,14 @@
+""" Example simple arithmetic use go """
+
+package main
+
+import (
+    "fmt"
+)
+
+func main() {
+    s := 80
+    t := 6
+    r := float64(s) / float64(t)
+    fmt.Println(r)
+}
